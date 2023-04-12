0a0813b6becbe084ec387b92395dda87368ef6eb
dnadawa <dulajnadawa@gmail.com>
2023-04-12 18:09:04 +0530

    Optimize the code

--- app/src/main/java/com/w1866973/meal_mate/SearchByIngredientsActivity.kt
+++ app/src/main/java/com/w1866973/meal_mate/SearchByIngredientsActivity.kt
@@ -1,17 +1,14 @@
 package com.w1866973.meal_mate
 
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
 import android.widget.LinearLayout
-import android.widget.ListView
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
-import androidx.room.Room
 import kotlinx.coroutines.*
 import org.json.JSONArray
 import org.json.JSONObject
@@ -20,7 +17,7 @@ import java.net.URL
 
 class SearchByIngredientsActivity : AppCompatActivity() {
 
-    val fetchedMealsList : ArrayList<Meal> = arrayListOf()
+    private val fetchedMealsList: ArrayList<Meal> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,52 +50,35 @@ class SearchByIngredientsActivity : AppCompatActivity() {
 
             CoroutineScope(Dispatchers.IO).launch {
                 try {
-                    val fetchedData: JSONObject =
-                        sendHttpGetRequest("https://www.themealdb.com/api/json/v1/1/filter.php?i=$searchText")
+                    val fetchedMeals: JSONArray =
+                        getMealsList("https://www.themealdb.com/api/json/v1/1/filter.php?i=$searchText")
 
-                    if (!fetchedData.isNull("meals")) {
-                        val mealsList: JSONArray = fetchedData.getJSONArray("meals")
+                    for (i in 0 until fetchedMeals.length()) {
+                        val meal: JSONObject = fetchedMeals[i] as JSONObject
+                        val id = meal.getString("idMeal")
 
-                        val idList: ArrayList<String> = arrayListOf()
-                        for (i in 0 until mealsList.length()) {
-                            val meal: JSONObject = mealsList[i] as JSONObject
-                            idList.add(meal.getString("idMeal"))
-                        }
+                        val detailedMeals: JSONArray =
+                            getMealsList("https://www.themealdb.com/api/json/v1/1/lookup.php?i=$id")
 
-
-                        for (id in idList) {
-                            val mealData: JSONObject = sendHttpGetRequest("https://www.themealdb.com/api/json/v1/1/lookup.php?i=$id")
-                            if (!mealData.isNull("meals")) {
-                                val mealsList: JSONArray = mealData.getJSONArray("meals")
-                                val meal: JSONObject = mealsList[0] as JSONObject
-                                val mealObj = Meal.fromJson(meal)
-                                fetchedMealsList.add(mealObj)
-                            }
-                        }
-
-                        withContext(Dispatchers.Main){
-                            progressBar.visibility = View.GONE
-
-                            for(meal in fetchedMealsList){
-                                val textView = LayoutInflater.from(applicationContext).inflate(R.layout.card, cardsListView, false) as TextView
-                                textView.text = meal.toString()
-                                cardsListView.addView(textView)
-                            }
-                        }
-
-                    } else {
-                        withContext(Dispatchers.Main) {
-                            Toast.makeText(
-                                applicationContext,
-                                "No data found!",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
+                        val fetchedDetailedMeal: JSONObject = detailedMeals[0] as JSONObject
+                        val mealObj: Meal = Meal.fromJson(fetchedDetailedMeal)
+                        fetchedMealsList.add(mealObj)
                     }
 
+                    withContext(Dispatchers.Main) {
+                        progressBar.visibility = View.GONE
 
+                        for (meal in fetchedMealsList) {
+                            val textView = LayoutInflater.from(applicationContext)
+                                .inflate(R.layout.card, cardsListView, false) as TextView
+                            textView.text = meal.toString()
+                            cardsListView.addView(textView)
+                        }
+                    }
                 } catch (e: Exception) {
                     withContext(Dispatchers.Main) {
+                        progressBar.visibility = View.GONE
+
                         Toast.makeText(
                             applicationContext,
                             e.message,
@@ -128,26 +108,39 @@ class SearchByIngredientsActivity : AppCompatActivity() {
         return JSONObject(response.toString())
     }
 
+    private fun getMealsList(url: String): JSONArray {
+        val fetchedData: JSONObject = sendHttpGetRequest(url)
+        if (fetchedData.isNull("meals")) {
+            throw Exception("No meals found!");
+        }
 
-    fun onSaveToDBButtonClicked(view: View){
-       val appDatabase: AppDatabase = AppDatabase.getDatabase(this)
+        return fetchedData.getJSONArray("meals")
+    }
+
+
+    fun onSaveToDBButtonClicked(view: View) {
+        val appDatabase: AppDatabase = AppDatabase.getDatabase(this)
         val mealDao: MealDao = appDatabase.mealDao()
 
-        if(fetchedMealsList.isEmpty()){
+        if (fetchedMealsList.isEmpty()) {
             Toast.makeText(applicationContext, "No meals fetched!", Toast.LENGTH_SHORT)
                 .show()
         } else {
 
-                CoroutineScope(Dispatchers.IO).launch {
-                    for(meal in fetchedMealsList){
-                        mealDao.insertMeals(meal)
-                    }
-
-                    withContext(Dispatchers.Main){
-                        Toast.makeText(applicationContext, "Successfully added to the database!", Toast.LENGTH_SHORT)
-                            .show()
-                    }
+            CoroutineScope(Dispatchers.IO).launch {
+                for (meal in fetchedMealsList) {
+                    mealDao.insertMeals(meal)
                 }
+
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Successfully added to the database!",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            }
         }
     }
 }
